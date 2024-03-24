833905681e5325243539b6c100364fd9d6229d76
bartosz25 <bartkonieczny@yahoo.fr>
2024-03-24 12:59:44 +0100

    add demo

---
+++ chapter-06/04-orchestration/02-concurrent-runner-airflow-ingestion/README.md
@@ -0,0 +1,28 @@
+# Orchestration - concurrent data ingestion
+
+1. Generate the dataset:
+```
+cd dataset
+mkdir -p /tmp/dedp/ch06/04-orchestration/02-concurrent-airflow-data-ingestion/input
+mkdir -p /tmp/dedp/ch06/04-orchestration/02-concurrent-airflow-data-ingestion/input-internal
+docker-compose down --volumes; docker-compose up
+```
+2. Start Apache Airflow instance:
+**⚠️ Enable the Virtual Environment before**
+```
+./start.sh
+```
+3. Access the Web UI at http://localhost:8080/login/ with dedp/dedp as login/password
+
+4. Open the [devices_loader.py](dags%2Fdevices_loader.py)
+* the pipeline can run concurrently in a safe manner because:
+  * each run works on a dedicated output location
+  * each run doesn't alter the input dataset which, for the purpose of this demo, is shared across the executions
+* more specifically, the DAG is configured as:
+  * running at most 5 different instances at the same time (`max_active_runs=5,`)
+  * running tasks in parallel as they don't depend on each other (`'depend_on_past': False,`)
+
+5. Enable the `devices_loader` on the UI. You should see multiple instances of the pipeline starting at the same time:
+![concurrent_runs.png](assets%2Fconcurrent_runs.png)
+
+The pipeline has two empty tasks only to better illustrate the parallel execution.
---
+++ chapter-06/04-orchestration/02-concurrent-runner-airflow-ingestion/dags/devices_loader.py
@@ -0,0 +1,47 @@
+import os
+import shutil
+from datetime import timedelta
+
+import pendulum
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import get_current_context
+from airflow.sensors.filesystem import FileSensor
+
+with DAG('devices_loader', max_active_runs=5,
+         default_args={
+             'depend_on_past': False,
+             'start_date': pendulum.now(tz='utc').subtract(days=4),
+             'retries': 3,
+             'retry_delay': timedelta(minutes=1)
+         },
+         schedule_interval="@daily") as dag:
+    input_devices_file = '/tmp/dedp/ch06/04-orchestration/02-concurrent-airflow-data-ingestion/input/dataset.csv'
+
+    input_data_sensor = FileSensor(
+        task_id='input_data_sensor',
+        filepath=input_devices_file,
+        mode='reschedule',
+        do_xcom_push=False
+    )
+
+    @task
+    def load_new_devices_to_internal_storage():
+        context = get_current_context()
+        internal_base_location = '/tmp/dedp/ch06/04-orchestration/02-concurrent-airflow-data-ingestion/input-internal'
+        partitioned_dir = f'{internal_base_location}/{context["ds_nodash"]}'
+        shutil.rmtree(partitioned_dir, ignore_errors=True)
+        os.mkdir(partitioned_dir)
+        internal_file_location = f'{partitioned_dir}/dataset.csv'
+        shutil.copyfile(input_devices_file, internal_file_location)
+
+    empty_task_1 = EmptyOperator(
+        task_id='empty_task_1'
+    )
+
+    empty_task_2 = EmptyOperator(
+        task_id='empty_task_2'
+    )
+
+    input_data_sensor >> load_new_devices_to_internal_storage() >> empty_task_1 >> empty_task_2
