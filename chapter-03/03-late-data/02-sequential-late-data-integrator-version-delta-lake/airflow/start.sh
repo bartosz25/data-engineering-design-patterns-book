89c764486c0dcad4d65047793ff33f369d820526
bartosz25 <bartkonieczny@yahoo.fr>
2024-12-24 05:14:37 +0100

    clean old dags

--- chapter-03/03-late-data/02-sequential-late-data-integrator-version-delta-lake/airflow/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-airflow db reset
-airflow db init
-airflow users create --username "dedp" --role "Admin" --password "dedp" --email "empty" --firstname "admin" --lastname "admin"
-export AIRFLOW__CORE__DAGS_FOLDER=./dags
-export AIRFLOW__CORE__LOAD_EXAMPLES=false
-airflow webserver & airflow scheduler
